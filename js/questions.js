0832b5332903274b7f82cd6e73e94cc7a3ff4a6e
Elias Vasquez <eliasvasquez7@gmail.com>
2022-03-16 23:44:13 -0600

    Added folders to their own, also added js and css, added comments as well

---
+++ js/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+    "Hyper Text Preprocessor",
+    "Hyper Text Markup Language",
+    "Hyper Text Multiple Language",
+    "Hyper Tool Multi Language"
+    ]
+},
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+    "Common Style Sheet",
+    "Colorful Style Sheet",
+    "Computer Style Sheet",
+    "Cascading Style Sheet"
+    ]
+},
+    {
+    numb: 3,
+    question: "What year was Javascript created?",
+    answer: "1995",
+    options: [
+    "1993",
+    "1995",
+    "1996",
+    "1994"
+    ]
+},
+    {
+    numb: 4,
+    question: "Arrays in JavaScript can be used to store ____?",
+    answer: "All of the above",
+    options: [
+    "Numbers and Strings",
+    "Other Arrays",
+    "Booleans",
+    "All of the above"
+    ]
+},
+    {
+    numb: 5,
+    question: "The condition in an if/else statement is enclosed within _____?",
+    answer: "Parentheses",
+    options: [
+    "Round Brackets",
+    "Square Brackets",
+    "Parentheses",
+    "Curly Brackets"
+    ]
+},
+
+];
